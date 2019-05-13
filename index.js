bc760706073720df497fe26f354675a6f705d43b
ryanaltair_quicklane <al_tair@live.com>
2019-05-13 22:18:03 +0800

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+const {
+    Random
+} = require("random-js");
+const random = new Random(); // uses the nativeMath engine
+const process = require('process')
+// const send = require('./send')
+let list = require('./list')
+let sum = 0
+let depth = 0
+for (const one of list) {
+    if (one.count > depth) {
+        depth = one.count
+    }
+}
+let votes = Math.pow(2, depth)
+let lastTrigger = 1;
+for (const one of list) {
+    let seed = votes - Math.pow(2, (depth - one.count))
+    if (seed === 0) {
+        seed = votes
+    }
+    sum += seed
+    one.start = lastTrigger
+    one.last = lastTrigger + seed - 1
+    one.seed = seed
+    one.rate = seed / votes
+    lastTrigger = lastTrigger + seed
+    console.log(`name: ${one.name}, seed: ${one.seed} , ${one.start}:${one.last}, rate: ${one.rate}`)
+}
+console.log('who we are', list.length)
+console.log('sum', sum, 'votes', votes, 'depth', depth)
+
+
+const value = random.integer(1, sum);
+console.log('get lucky num ', value)
+for (const one of list) {
+    if (value <= one.last) {
+        console.log('now we get', one)
+        console.log('now 恭喜', one.name)
+        // send(one.name)
+        process.exit()
+    }
+}
---
+++ list.js
@@ -0,0 +1,45 @@
+module.exports = [{
+        name: '河鼓',
+        count: 0,
+        disable: false
+    },
+    {
+        name: '奔豚',
+        count: 0,
+        disable: false
+    },
+    {
+        name: '楚非',
+        count: 0,
+        disable: false
+    },
+    {
+        name: '小靖',
+        count: 0,
+        disable: false
+    },
+    {
+        name: '梅清',
+        count: 0,
+        disable: false
+    },
+    {
+        name: '秋水',
+        count: 1,
+        disable: false
+    },
+    {
+        name: '李白',
+        count: 0,
+        disable: false
+    },
+    {
+        name: '墨文',
+        count: 0,
+        disable: false
+    }, {
+        name: '玄策',
+        count: 0,
+        disable: false
+    },
+]
